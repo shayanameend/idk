3ebfc40610cb93402ac3a41371d0923f071e30cd
shayanameen <shayan.ameen.developer@gmail.com>
2024-11-02 16:43:02 +0500

    Update contact form attributes and styling for improved functionality and consistency

--- src/components/svelte/contact-section.svelte
+++ src/components/svelte/contact-section.svelte
@@ -33,18 +33,18 @@ useEffect(() => {
       class="opacity-0 translate-y-full px-4 mb-12 flex flex-col lg:flex-row gap-12 lg:gap-24"
     >
       <div class="flex-1 space-y-4">
-        <form class="mt-10" name="contact" netlify>
+        <form class="mt-10" name="contact" method="POST" data-netlify="true">
           <div class="grid gap-6 sm:grid-cols-2">
             <div class="relative z-0">
-              <input type="text" id="name" name="name" class="peer block w-full appearance-none border-0 border-b border-gray-500 bg-transparent py-2.5 px-0 text-sm text-gray-900 focus:border-[#fe4164] focus:outline-none focus:ring-0" placeholder=" " />
+              <input type="text" id="name" name="name" class="peer block w-full appearance-none border-0 border-b border-[#fe4164] bg-transparent py-2.5 px-0 text-sm text-gray-900 focus:border-[#fe4164] focus:outline-none focus:ring-0" placeholder=" " />
               <label for="name" class="absolute top-3 -z-10 origin-[0] -translate-y-6 scale-75 transform text-sm text-gray-500 duration-300 peer-placeholder-shown:translate-y-0 peer-placeholder-shown:scale-100 peer-focus:left-0 peer-focus:-translate-y-6 peer-focus:scale-75 peer-focus:text-[#fe4164] peer-focus:dark:text-[#fe4164]">Your name</label>
             </div>
             <div class="relative z-0">
-              <input type="text" id="email" name="email" class="peer block w-full appearance-none border-0 border-b border-gray-500 bg-transparent py-2.5 px-0 text-sm text-gray-900 focus:border-[#fe4164] focus:outline-none focus:ring-0" placeholder=" " />
+              <input type="text" id="email" name="email" class="peer block w-full appearance-none border-0 border-b border-[#fe4164] bg-transparent py-2.5 px-0 text-sm text-gray-900 focus:border-[#fe4164] focus:outline-none focus:ring-0" placeholder=" " />
               <label for="email" class="absolute top-3 -z-10 origin-[0] -translate-y-6 scale-75 transform text-sm text-gray-500 duration-300 peer-placeholder-shown:translate-y-0 peer-placeholder-shown:scale-100 peer-focus:left-0 peer-focus:-translate-y-6 peer-focus:scale-75 peer-focus:text-[#fe4164] peer-focus:dark:text-[#fe4164]">Your email</label>
             </div>
             <div class="relative z-0 col-span-2">
-              <textarea id="message" name="message" rows="5" class="peer block w-full appearance-none border-0 border-b border-gray-500 bg-transparent py-2.5 px-0 text-sm text-gray-900 focus:border-[#fe4164] focus:outline-none focus:ring-0" placeholder=" "></textarea>
+              <textarea id="message" name="message" rows="5" class="peer block w-full appearance-none border-0 border-b border-[#fe4164] bg-transparent py-2.5 px-0 text-sm text-gray-900 focus:border-[#fe4164] focus:outline-none focus:ring-0" placeholder=" "></textarea>
               <label for="message" class="absolute top-3 -z-10 origin-[0] -translate-y-6 scale-75 transform text-sm text-gray-500 duration-300 peer-placeholder-shown:translate-y-0 peer-placeholder-shown:scale-100 peer-focus:left-0 peer-focus:-translate-y-6 peer-focus:scale-75 peer-focus:text-[#fe4164] peer-focus:dark:text-[#fe4164]">Your message</label>
             </div>
           </div>
